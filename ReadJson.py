4d35a4401eade629f97401442964be0afa240436
Vinicius Kopcheski <viniciusfk@gmail.com>
2024-02-24 17:59:15 +0100

    small fix

--- ReadJson.py
+++ ReadJson.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, datediff, current_date
+from pyspark.sql.functions import col, datediff, current_date, lit
 
 # Spark session & context
 spark = (SparkSession.builder
@@ -14,6 +14,6 @@ df.show(10, False)
 
 (df.filter(df.profession == "Footballer")
     .sort(col("date_of_birth"))
-    .withColumn("current_age", datediff(current_date(), col("date_of_birth")) / 365)
+    .withColumn("current_age", lit(datediff(current_date(), col("date_of_birth")) / 365))
     .withColumn("current_age", col("current_age").cast("int"))
     .show(10, False, True))
